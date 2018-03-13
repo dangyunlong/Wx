7b3e34099fc52f4e19e8e3c57517d63b2b62a9fb
Sean <381332123@qq.com>
2018-03-13 22:26:02 +0800

    picker
    picker

--- pages/bill/bill.js
+++ pages/bill/bill.js
@@ -3,6 +3,7 @@ Page({
 
   data: {
     stateimg:"/images/state/true.png",
+    index:"0",
     listdata: [
       { allmoney: "2,000.00", time: "2017-12-12 13:14", img: "/images/state/state1.png", money: "2,000.00", mmoney:"1,000.00",num:"12"},
       { allmoney: "3,000.00", time: "2017-12-11 10:00", img: "/images/state/state2.png", money: "1,800.00", mmoney: "900.00", num: "6" },
@@ -11,12 +12,15 @@ Page({
       { allmoney: "990.00", time: "2017-11-30 06:40", img: "/images/state/state2.png", money: "100.00", mmoney: "300.00", num: "6" },
     ],
     qi: ["3期","6期","12期","24期","32期"],
-    bindPickerChange: function (e) {
-      console.log('picker发送选择改变，携带值为', e.detail.value)
-      this.setData({
-        index: e.detail.value
-      })
-    },
-  }
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+
+
 
 })  
